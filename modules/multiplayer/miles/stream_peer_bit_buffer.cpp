c7538c32894c4ee127960058304bbc99e6e85d23
ExplodingImplosion <60752903+ExplodingImplosion@users.noreply.github.com>
2026-03-09 13:48:05 -0400

    asdf

--- modules/multiplayer/miles/stream_peer_bit_buffer.cpp
+++ modules/multiplayer/miles/stream_peer_bit_buffer.cpp
@@ -1,209 +1,222 @@
-#include stream_peer_bit_buffer.h
+#include "stream_peer_bit_buffer.h"
 String _to_string() {
 	
 }
-void reset() {
-	
+void StreamPeerBitBuffer::reset() {
+	bool_position = 0;
+    seek(bool_bytes);
 }
-void _init(int with_size, int allocated_bools) {
-	
+// NOTE: BUG! reallocating bools turns var_pos negative (wtf?)
+void StreamPeerBitBuffer::_init(int with_size, int allocated_bools) {
+	bool_bytes = (allocated_bools+7) / 8;
+    num_allocated_bools = bool_bytes * 8;
+    resize(with_size);
+    seek(bool_bytes);
 }
-PackedByteArray export(bool until_position) {
-	
+PackedByteArray StreamPeerBitBuffer::export(bool until_position) {
+	PackedByteArray export_data;
+    export_data.resize(4);
+    export_data.encode_u32(0,bool_position);
+    export_data.append_array(get_bools(until_position));
+    export_data.append_array(get_non_bools(until_position));
 }
-void import(PackedByteArray bytes) {
-	
+void StreamPeerBitBuffer::import(PackedByteArray bytes) {
+	uint max_bool_idx = bytes.decode_u32(0);
+    data = bytes.slice(4);
+    bool_bytes = (max_bool_idx+7) / 8;
+    num_allocated_bools = bool_bytes * 8;
+    reset();
 }
-PackedByteArray get_bools(bool until_position) {
-	
+PackedByteArray StreamPeerBitBuffer::get_bools(bool until_position) {
+	return until_position ? data.slice(0,bool_position + 7) / 8 : bool_bytes;
 }
-static StreamPeerBuffer decode(PackedByteArray bytes) {
+static StreamPeerBuffer StreamPeerBitBuffer::decode(PackedByteArray bytes) {
 	
 }
-PackedByteArray get_non_bools(bool until_position) {
+PackedByteArray StreamPeerBitBuffer::get_non_bools(bool until_position) {
 	
 }
-Array get_bools_as_array() {
+Array StreamPeerBitBuffer::get_bools_as_array() {
 	
 }
-void ensure_bools_allocated() {
+void StreamPeerBitBuffer::ensure_bools_allocated() {
 	
 }
-void reallocate_bools(int amount) {
+void StreamPeerBitBuffer::reallocate_bools(int amount) {
 	
 }
-void put_bool(bool value) {
+void StreamPeerBitBuffer::put_bool(bool value) {
 	
 }
-bool put_eval(bool evaluation) {
+bool StreamPeerBitBuffer::put_eval(bool evaluation) {
 	
 }
-bool get_bool() {
+bool StreamPeerBitBuffer::get_bool() {
 	
 }
-void seek_var_pos(int position) {
+void StreamPeerBitBuffer::seek_var_pos(int position) {
 	
 }
-int get_var_pos() {
+int StreamPeerBitBuffer::get_var_pos() {
 	
 }
-void jump(int amount) {
+void StreamPeerBitBuffer::jump(int amount) {
 	
 }
-void put_v2(Vector2 v2) {
+void StreamPeerBitBuffer::put_v2(Vector2 v2) {
 	
 }
-Vector2 get_v2() {
+Vector2 StreamPeerBitBuffer::get_v2() {
 	
 }
-void put_v3(Vector3 v3) {
+void StreamPeerBitBuffer::put_v3(Vector3 v3) {
 	
 }
-Vector3 get_v3() {
+Vector3 StreamPeerBitBuffer::get_v3() {
 	
 }
-void put_v4(Vector4 v4) {
+void StreamPeerBitBuffer::put_v4(Vector4 v4) {
 	
 }
-Vector4 get_v4() {
+Vector4 StreamPeerBitBuffer::get_v4() {
 	
 }
-void put_v2i(Vector2i v2) {
+void StreamPeerBitBuffer::put_v2i(Vector2i v2) {
 	
 }
-Vector2i get_v2i() {
+Vector2i StreamPeerBitBuffer::get_v2i() {
 	
 }
-void put_v3i(Vector3i v3) {
+void StreamPeerBitBuffer::put_v3i(Vector3i v3) {
 	
 }
-Vector3i get_v3i() {
+Vector3i StreamPeerBitBuffer::get_v3i() {
 	
 }
-void put_v4i(Vector4i v4) {
+void StreamPeerBitBuffer::put_v4i(Vector4i v4) {
 	
 }
-Vector4i get_v4i() {
+Vector4i StreamPeerBitBuffer::get_v4i() {
 	
 }
-void put_r2(Rect2 r2) {
+void StreamPeerBitBuffer::put_r2(Rect2 r2) {
 	
 }
-Rect2 get_r2() {
+Rect2 StreamPeerBitBuffer::get_r2() {
 	
 }
-void put_r2i(Rect2i r2i) {
+void StreamPeerBitBuffer::put_r2i(Rect2i r2i) {
 	
 }
-Rect2i get_r2i() {
+Rect2i StreamPeerBitBuffer::get_r2i() {
 	
 }
-void put_quat(Quaternion quat) {
+void StreamPeerBitBuffer::put_quat(Quaternion quat) {
 	
 }
-Quaternion get_quat() {
+Quaternion StreamPeerBitBuffer::get_quat() {
 	
 }
-void put_color(Color color) {
+void StreamPeerBitBuffer::put_color(Color color) {
 	
 }
-Color get_color() {
+Color StreamPeerBitBuffer::get_color() {
 	
 }
-void put_udynamic(int num) {
+void StreamPeerBitBuffer::put_udynamic(int num) {
 	
 }
-int get_udynamic() {
+int StreamPeerBitBuffer::get_udynamic() {
 	
 }
-void put_dynamic(int num) {
+void StreamPeerBitBuffer::put_dynamic(int num) {
 	
 }
-int get_dynamic() {
+int StreamPeerBitBuffer::get_dynamic() {
 	
 }
-void put_n8(float num, float unit) {
+void StreamPeerBitBuffer::put_n8(float num, float unit) {
 	
 }
-float get_n8(float unit) {
+float StreamPeerBitBuffer::get_n8(float unit) {
 	
 }
-void put_n16(float num, float unit) {
+void StreamPeerBitBuffer::put_n16(float num, float unit) {
 	
 }
-float get_n16(float unit) {
+float StreamPeerBitBuffer::get_n16(float unit) {
 	
 }
-void put_n32(float num, float unit) {
+void StreamPeerBitBuffer::put_n32(float num, float unit) {
 	
 }
-float get_n32(float unit) {
+float StreamPeerBitBuffer::get_n32(float unit) {
 	
 }
-void put_n64(float num, float unit) {
+void StreamPeerBitBuffer::put_n64(float num, float unit) {
 	
 }
-float get_n64(float unit) {
+float StreamPeerBitBuffer::get_n64(float unit) {
 	
 }
-void put_r8(float num, float unit) {
+void StreamPeerBitBuffer::put_r8(float num, float unit) {
 	
 }
-float get_r8(float unit) {
+float StreamPeerBitBuffer::get_r8(float unit) {
 	
 }
-void put_r16(float num, float unit) {
+void StreamPeerBitBuffer::put_r16(float num, float unit) {
 	
 }
-float get_r16(float unit) {
+float StreamPeerBitBuffer::get_r16(float unit) {
 	
 }
-void put_r32(float num, float unit) {
+void StreamPeerBitBuffer::put_r32(float num, float unit) {
 	
 }
-float get_r32(float unit) {
+float StreamPeerBitBuffer::get_r32(float unit) {
 	
 }
-void put_r64(float num, float unit) {
+void StreamPeerBitBuffer::put_r64(float num, float unit) {
 	
 }
-float get_r64(float unit) {
+float StreamPeerBitBuffer::get_r64(float unit) {
 	
 }
-void put_rotation(Vector3 rot) {
+void StreamPeerBitBuffer::put_rotation(Vector3 rot) {
 	
 }
-Vector3 get_rotation() {
+Vector3 StreamPeerBitBuffer::get_rotation() {
 	
 }
-void put_rotation_half(Vector3 rot) {
+void StreamPeerBitBuffer::put_rotation_half(Vector3 rot) {
 	
 }
-Vector3 get_rotation_half() {
+Vector3 StreamPeerBitBuffer::get_rotation_half() {
 	
 }
-void put_nv2(Vector2 v2, float unit) {
+void StreamPeerBitBuffer::put_nv2(Vector2 v2, float unit) {
 	
 }
-Vector2 get_nv2(float unit) {
+Vector2 StreamPeerBitBuffer::get_nv2(float unit) {
 	
 }
-void put_rv2_half(Vector2 v2, float unit) {
+void StreamPeerBitBuffer::put_rv2_half(Vector2 v2, float unit) {
 	
 }
-Vector2 get_rv2_half(float unit) {
+Vector2 StreamPeerBitBuffer::get_rv2_half(float unit) {
 	
 }
-void put_nv2_half(Vector2 v2, float unit) {
+void StreamPeerBitBuffer::put_nv2_half(Vector2 v2, float unit) {
 	
 }
-Vector2 get_nv2_half(float unit) {
+Vector2 StreamPeerBitBuffer::get_nv2_half(float unit) {
 	
 }
-void put_probabalistic_enum(int value) {
+void StreamPeerBitBuffer::put_probabalistic_enum(int value) {
 	
 }
-int get_probabalistic_enum(int enum_max) {
+int StreamPeerBitBuffer::get_probabalistic_enum(int enum_max) {
 	
 }
 void StreamPeerBitBuffer::_bind_methods(){
