c78a0801560fb524a2e2cc0518622c2ed25c0bbc
ExplodingImplosion <explodingimplosion14@gmail.com>
2026-03-28 16:26:10 -0400

    settings

--- modules/multiplayer/miles/settings.cpp
+++ modules/multiplayer/miles/settings.cpp
@@ -1,13 +1,20 @@
 #include "settings.h"
-Settings::Settings(){
+// Settings::Settings(){
 	
-}
-Settings::~Settings(){
+// }
+// Settings::~Settings(){
 	
-}
-void Settings::get_setting_safe(String name, void default_value) {
-	
+// }
+Variant Settings::get_setting_safe(String name, Variant default_value) {
+	auto ps = ProjectSettings::get_singleton();
+    if (ps->has_setting(name)) {
+        return ps->get_setting(name);
+    }
+    else {
+        ps->set_setting(name,default_value);
+        return default_value;
+    }
 }
 void Settings::_bind_methods(){
-ClassDB::bind_static_method("Settings", D_METHOD("get_setting_safe", "name", "default_value"), &Settings::get_setting_safe, DEFVAL(<null>));
+    ClassDB::bind_static_method("Settings", D_METHOD("get_setting_safe", "name", "default_value"), &Settings::get_setting_safe, DEFVAL(nullptr));
 }
--- modules/multiplayer/miles/settings.h
+++ modules/multiplayer/miles/settings.h
@@ -1,8 +1,10 @@
+#include "core/config/project_settings.h"
+#include "core/object/class_db.h"
 
 class Settings: public RefCounted {
-GDCLASS(Settings, RefCounted);
-public:
-static void get_setting_safe(String name, void default_value);
-static void _bind_methods();
-Settings();~Settings();
+    GDCLASS(Settings, RefCounted);
+    public:
+        static Variant get_setting_safe(String name, Variant default_value);
+        static void _bind_methods();
+        // Settings();~Settings();
 };
