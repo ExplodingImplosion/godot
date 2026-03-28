b373e0515368f0377262027c2fd1604c4bb914cf
ExplodingImplosion <explodingimplosion14@gmail.com>
2026-03-28 16:26:32 -0400

    debug meshes (unfinished)

--- modules/multiplayer/miles/debug_meshes.cpp
+++ modules/multiplayer/miles/debug_meshes.cpp
@@ -1,76 +1,111 @@
 #include "debug_meshes.h"
-DebugMeshes::DebugMeshes(){
+// DebugMeshes::DebugMeshes(){
+	
+// }
+// DebugMeshes::~DebugMeshes(){
+	
+// }
+void DebugMeshes::add_debug_boxmesh(Transform3D transform, float time, Vector3 custom_extents) {
+	Ref<BoxMesh> bm; bm.instantiate();
+	bm->set_size(custom_extents);
+	spawn_debug_mesh(bm,transform,time);
+}
+
+MeshInstance3D* DebugMeshes::spawn_debug_raycast_mesh(RayCast3D* raycast, float time) {
+	// if (raycast_debug_mesh) {
+	// 	return raycast_debug_mesh;
+	// }
+	// else {
+	// 	Ref<StandardMaterial3D> mat; mat.instantiate();
+	// 	mat->set_shading_mode(BaseMaterial3D::SHADING_MODE_UNSHADED);
+	// 	mat->set_flag(BaseMaterial3D::FLAG_DISABLE_FOG,true);
+	// 	mat->set_cull_mode(BaseMaterial3D::CULL_DISABLED);
+	// 	mat->set_transparency(BaseMaterial3D::TRANSPARENCY_ALPHA);
+	// 	mat->set_albedo(Color(0.,1.,0.,1.));
+
+	// }
+}
+
+MeshInstance3D* DebugMeshes::draw_line(Vector3 start, Vector3 end, float lifetime, Color color) {
 	
 }
-DebugMeshes::~DebugMeshes(){
-	
+
+MeshInstance3D* DebugMeshes::spawn_debug_mesh(Ref<Mesh> mesh, Transform3D transform, float time) {
+	MeshInstance3D* node = memnew(MeshInstance3D);
+	node->set_mesh(mesh);
+	node->connect(StringName("ready"),Callable(node,StringName("set_global_transform")).bind(transform),CONNECT_ONE_SHOT);
+	auto tree = node->get_tree();
+	tree->get_current_scene()->add_child(node);
+	node->add_to_group(StringName("Debug Collision Shapes"));
+	tree->create_timer(time,true,true,false)->connect(StringName("timeout"),Callable(node,StringName("queue_free")),CONNECT_ONE_SHOT);
 }
-void DebugMeshes::add_debug_boxmesh(Transform3d transform, float time, Vector3 custom_extents) {
-	
-}
-MeshInstance3D DebugMeshes::spawn_debug_raycast_mesh(RayCast3D raycast, float time) {
-	
-}
-MeshInstance3D DebugMeshes::draw_line(Vector3 start, Vector3 end, float lifetime, Color color) {
-	
-}
-MeshInstance3D DebugMeshes::spawn_debug_mesh(Mesh mesh, Transform3d transform, float time) {
-	
-}
+
 bool DebugMeshes::can_add_debug_mesh() {
-	
+	return Settings::get_setting_safe("quack/debug/show_collisions",false);
 }
+
 bool DebugMeshes::can_add_debug_ray() {
+	return Settings::get_setting_safe("quack/debug/show_rays",false);
+}
+
+MeshInstance3D* DebugMeshes::spawn_colldier_debug_mesh(CollisionShape3D* collider, float time) {
 	
 }
-MeshInstance3D DebugMeshes::spawn_colldier_debug_mesh(CollisionShape3D collider, float time) {
+
+MeshInstance3D* DebugMeshes::spawn_recolored_debug_mesh(Ref<Mesh> mesh, Color color, Transform3D transform, float time) {
 	
 }
-MeshInstance3D DebugMeshes::spawn_recolored_debug_mesh(Mesh mesh, Color color, Transform3d transform, float time) {
+
+MeshInstance3D* DebugMeshes::spawn_recolored_colldier_debug_mesh(CollisionShape3D* collider, Color color, float time) {
 	
 }
-MeshInstance3D DebugMeshes::spawn_recolored_colldier_debug_mesh(CollisionShape3D collider, Color color, float time) {
+
+void DebugMeshes::spawn_debug_mesh_child(CollisionShape3D* collider, bool interp) {
 	
 }
-void DebugMeshes::spawn_debug_mesh_child(CollisionShape3D collider, bool interp) {
-	
-}
-static StandardMaterial3D DebugMeshes::get_raycast_debug_mesh() {
+
+Ref<StandardMaterial3D> DebugMeshes::get_raycast_debug_mesh() {
 	return raycast_debug_mesh;
 }
-static void DebugMeshes::set_raycast_debug_mesh(StandardMaterial3D p_raycast_debug_mesh) {
+
+void DebugMeshes::set_raycast_debug_mesh(Ref<StandardMaterial3D> p_raycast_debug_mesh) {
 	raycast_debug_mesh = p_raycast_debug_mesh;
 }
-static StandardMaterial3D DebugMeshes::get_raycast_colliding_debug_mesh() {
+
+Ref<StandardMaterial3D> DebugMeshes::get_raycast_colliding_debug_mesh() {
 	return raycast_colliding_debug_mesh;
 }
-static void DebugMeshes::set_raycast_colliding_debug_mesh(StandardMaterial3D p_raycast_colliding_debug_mesh) {
+
+void DebugMeshes::set_raycast_colliding_debug_mesh(Ref<StandardMaterial3D> p_raycast_colliding_debug_mesh) {
 	raycast_colliding_debug_mesh = p_raycast_colliding_debug_mesh;
 }
-static ArrayMesh DebugMeshes::get_impact_mesh() {
+
+Ref<ArrayMesh> DebugMeshes::get_impact_mesh() {
 	return impact_mesh;
 }
-static void DebugMeshes::set_impact_mesh(ArrayMesh p_impact_mesh) {
+
+void DebugMeshes::set_impact_mesh(Ref<ArrayMesh> p_impact_mesh) {
 	impact_mesh = p_impact_mesh;
 }
+
 void DebugMeshes::_bind_methods(){
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("add_debug_boxmesh", "transform", "time", "custom_extents"), &DebugMeshes::add_debug_boxmesh, DEFVAL(3.0), DEFVAL((0.05, 0.05, 0.05)));
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_debug_raycast_mesh", "raycast", "time"), &DebugMeshes::spawn_debug_raycast_mesh, DEFVAL(3.0));
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("draw_line", "start", "end", "lifetime", "color"), &DebugMeshes::draw_line, DEFVAL((0.6275, 0.1255, 0.9412, 1.0)));
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_debug_mesh", "mesh", "transform", "time"), &DebugMeshes::spawn_debug_mesh, DEFVAL(3.0));
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("can_add_debug_mesh"), &DebugMeshes::can_add_debug_mesh);
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("can_add_debug_ray"), &DebugMeshes::can_add_debug_ray);
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_colldier_debug_mesh", "collider", "time"), &DebugMeshes::spawn_colldier_debug_mesh, DEFVAL(<null>));
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_recolored_debug_mesh", "mesh", "color", "transform", "time"), &DebugMeshes::spawn_recolored_debug_mesh);
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_recolored_colldier_debug_mesh", "collider", "color", "time"), &DebugMeshes::spawn_recolored_colldier_debug_mesh, DEFVAL((0.0, 0.6, 0.698, 0.0235)), DEFVAL(<null>));
-ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_debug_mesh_child", "collider", "interp"), &DebugMeshes::spawn_debug_mesh_child, DEFVAL(false));
-ClassDB::bind_method(D_METHOD("set_raycast_debug_mesh", "value"), &DebugMeshes::set_raycast_debug_mesh);
-ClassDB::bind_method(D_METHOD("get_raycast_debug_mesh"), &DebugMeshes::get_raycast_debug_mesh);
-ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "raycast_debug_mesh"), "set_raycast_debug_mesh", "get_raycast_debug_mesh"); // unfinished and u should prolly change this
-ClassDB::bind_method(D_METHOD("set_raycast_colliding_debug_mesh", "value"), &DebugMeshes::set_raycast_colliding_debug_mesh);
-ClassDB::bind_method(D_METHOD("get_raycast_colliding_debug_mesh"), &DebugMeshes::get_raycast_colliding_debug_mesh);
-ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "raycast_colliding_debug_mesh"), "set_raycast_colliding_debug_mesh", "get_raycast_colliding_debug_mesh"); // unfinished and u should prolly change this
-ClassDB::bind_method(D_METHOD("set_impact_mesh", "value"), &DebugMeshes::set_impact_mesh);
-ClassDB::bind_method(D_METHOD("get_impact_mesh"), &DebugMeshes::get_impact_mesh);
-ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "impact_mesh"), "set_impact_mesh", "get_impact_mesh"); // unfinished and u should prolly change this
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("add_debug_boxmesh", "transform", "time", "custom_extents"), &DebugMeshes::add_debug_boxmesh, DEFVAL(3.0), DEFVAL((0.05, 0.05, 0.05)));
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_debug_raycast_mesh", "raycast", "time"), &DebugMeshes::spawn_debug_raycast_mesh, DEFVAL(3.0));
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("draw_line", "start", "end", "lifetime", "color"), &DebugMeshes::draw_line, DEFVAL((0.6275, 0.1255, 0.9412, 1.0)));
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_debug_mesh", "mesh", "transform", "time"), &DebugMeshes::spawn_debug_mesh, DEFVAL(3.0));
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("can_add_debug_mesh"), &DebugMeshes::can_add_debug_mesh);
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("can_add_debug_ray"), &DebugMeshes::can_add_debug_ray);
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_colldier_debug_mesh", "collider", "time"), &DebugMeshes::spawn_colldier_debug_mesh, DEFVAL(<null>));
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_recolored_debug_mesh", "mesh", "color", "transform", "time"), &DebugMeshes::spawn_recolored_debug_mesh);
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_recolored_colldier_debug_mesh", "collider", "color", "time"), &DebugMeshes::spawn_recolored_colldier_debug_mesh, DEFVAL((0.0, 0.6, 0.698, 0.0235)), DEFVAL(<null>));
+	ClassDB::bind_static_method("DebugMeshes", D_METHOD("spawn_debug_mesh_child", "collider", "interp"), &DebugMeshes::spawn_debug_mesh_child, DEFVAL(false));
+	ClassDB::bind_method(D_METHOD("set_raycast_debug_mesh", "value"), &DebugMeshes::set_raycast_debug_mesh);
+	ClassDB::bind_method(D_METHOD("get_raycast_debug_mesh"), &DebugMeshes::get_raycast_debug_mesh);
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "raycast_debug_mesh"), "set_raycast_debug_mesh", "get_raycast_debug_mesh"); // unfinished and u should prolly change this
+	ClassDB::bind_method(D_METHOD("set_raycast_colliding_debug_mesh", "value"), &DebugMeshes::set_raycast_colliding_debug_mesh);
+	ClassDB::bind_method(D_METHOD("get_raycast_colliding_debug_mesh"), &DebugMeshes::get_raycast_colliding_debug_mesh);
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "raycast_colliding_debug_mesh"), "set_raycast_colliding_debug_mesh", "get_raycast_colliding_debug_mesh"); // unfinished and u should prolly change this
+	// ClassDB::bind_method(D_METHOD("set_impact_mesh", "value"), &DebugMeshes::set_impact_mesh);
+	ClassDB::bind_method(D_METHOD("get_impact_mesh"), &DebugMeshes::get_impact_mesh);
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "impact_mesh"), "set_impact_mesh", "get_impact_mesh"); // unfinished and u should prolly change this
 }
--- modules/multiplayer/miles/debug_meshes.h
+++ modules/multiplayer/miles/debug_meshes.h
@@ -1,29 +1,35 @@
-#include "stupid_4_pt_5_mesh.h"
+// #include "core/object/ref_counted.h"
+// #include "scene/resources/mesh.h"
+#include "scene/resources/3d/primitive_meshes.h"
+#include "scene/3d/mesh_instance_3d.h"
+#include "scene/3d/physics/ray_cast_3d.h"
+#include "scene/3d/physics/collision_shape_3d.h"
+#include "settings.h"
 
 class DebugMeshes: public RefCounted {
-GDCLASS(DebugMeshes, RefCounted);
-public:
-static StandardMaterial3D raycast_debug_mesh;
-static StandardMaterial3D raycast_colliding_debug_mesh;
-static ArrayMesh impact_mesh;
-const Color DEFAULT_DEBUG_COLLISION_COLOR = (0.0, 0.6, 0.702, 0.4196);
-const Color DEFAULT_DEBUG_COLLISION_VERTEX_COLOR = (0.0, 0.6, 0.698, 0.0235);
-static void add_debug_boxmesh(Transform3d transform, float time, Vector3 custom_extents);
-static MeshInstance3D spawn_debug_raycast_mesh(RayCast3D raycast, float time);
-static MeshInstance3D draw_line(Vector3 start, Vector3 end, float lifetime, Color color);
-static MeshInstance3D spawn_debug_mesh(Mesh mesh, Transform3d transform, float time);
-static bool can_add_debug_mesh();
-static bool can_add_debug_ray();
-static MeshInstance3D spawn_colldier_debug_mesh(CollisionShape3D collider, float time);
-static MeshInstance3D spawn_recolored_debug_mesh(Mesh mesh, Color color, Transform3d transform, float time);
-static MeshInstance3D spawn_recolored_colldier_debug_mesh(CollisionShape3D collider, Color color, float time);
-static void spawn_debug_mesh_child(CollisionShape3D collider, bool interp);
-static StandardMaterial3D get_raycast_debug_mesh();
-static void set_raycast_debug_mesh(StandardMaterial3D p_raycast_debug_mesh);
-static StandardMaterial3D get_raycast_colliding_debug_mesh();
-static void set_raycast_colliding_debug_mesh(StandardMaterial3D p_raycast_colliding_debug_mesh);
-static ArrayMesh get_impact_mesh();
-static void set_impact_mesh(ArrayMesh p_impact_mesh);
-static void _bind_methods();
-DebugMeshes();~DebugMeshes();
+    GDCLASS(DebugMeshes, RefCounted);
+    public:
+        static Ref<StandardMaterial3D> raycast_debug_mesh;
+        static Ref<StandardMaterial3D> raycast_colliding_debug_mesh;
+        static Ref<ArrayMesh> impact_mesh;
+        const Color DEFAULT_DEBUG_COLLISION_COLOR = Color(0.0, 0.6, 0.702, 0.4196);
+        const Color DEFAULT_DEBUG_COLLISION_VERTEX_COLOR = Color(0.0, 0.6, 0.698, 0.0235);
+        static void add_debug_boxmesh(Transform3D transform, float time, Vector3 custom_extents);
+        static MeshInstance3D* spawn_debug_raycast_mesh(RayCast3D* raycast, float time);
+        static MeshInstance3D* draw_line(Vector3 start, Vector3 end, float lifetime, Color color);
+        static MeshInstance3D* spawn_debug_mesh(Ref<Mesh>mesh, Transform3D transform, float time);
+        static bool can_add_debug_mesh();
+        static bool can_add_debug_ray();
+        static MeshInstance3D* spawn_colldier_debug_mesh(CollisionShape3D* collider, float time);
+        static MeshInstance3D* spawn_recolored_debug_mesh(Ref<Mesh>mesh, Color color, Transform3D transform, float time);
+        static MeshInstance3D* spawn_recolored_colldier_debug_mesh(CollisionShape3D* collider, Color color, float time);
+        static void spawn_debug_mesh_child(CollisionShape3D* collider, bool interp);
+        static Ref<StandardMaterial3D> get_raycast_debug_mesh();
+        static void set_raycast_debug_mesh(Ref<StandardMaterial3D> p_raycast_debug_mesh);
+        static Ref<StandardMaterial3D> get_raycast_colliding_debug_mesh();
+        static void set_raycast_colliding_debug_mesh(StandardMaterial3D* p_raycast_colliding_debug_mesh);
+        static Ref<ArrayMesh> get_impact_mesh();
+        static void set_impact_mesh(Ref<ArrayMesh> p_impact_mesh);
+        static void _bind_methods();
+        // DebugMeshes();~DebugMeshes();
 };
