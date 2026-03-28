c07278dc360574326bdf99214d562deb33d82221
ExplodingImplosion <explodingimplosion14@gmail.com>
2026-03-28 16:26:22 -0400

    4.5 (maybe unfinished)

--- modules/multiplayer/miles/stupid_4_pt_5_mesh.cpp
+++ modules/multiplayer/miles/stupid_4_pt_5_mesh.cpp
@@ -1,30 +1,54 @@
 #include "stupid_4_pt_5_mesh.h"
 Stupid4Pt5Mesh::Stupid4Pt5Mesh(){
-	
+	set_physics_interpolation_mode(PhysicsInterpolationMode::PHYSICS_INTERPOLATION_MODE_OFF);
+	set_as_top_level(true);
+	set_process_mode(PROCESS_MODE_PAUSABLE);
 }
-Stupid4Pt5Mesh::~Stupid4Pt5Mesh(){
+// Stupid4Pt5Mesh::~Stupid4Pt5Mesh(){
 	
-}
-void Stupid4Pt5Mesh::_init(CollisionShape3D collider, bool interp) {
-	
-}
-void Stupid4Pt5Mesh::_physics_process(float _delta) {
-	
-}
+// }
 void Stupid4Pt5Mesh::update_transform() {
-	
+	if (parent_collider) { set_global_transform(parent_collider->get_global_transform()); }
 }
-CollisionShape3D Stupid4Pt5Mesh::get_parent_collider() {
-	return parent_collider;
+
+
+
+void Stupid4Pt5Mesh::_notification(int what) {
+	switch (what) {
+		// case NOTIFICATION_POSTINITIALIZE: {
+		// 	break;
+		// }
+		case NOTIFICATION_PHYSICS_PROCESS: {
+			if (is_set_as_top_level()) {
+				callable_mp(this, &Stupid4Pt5Mesh::update_transform).call_deferred();
+			}
+			break;
+		}
+		case NOTIFICATION_PARENTED: {
+			parent_collider = Object::cast_to<CollisionShape3D>(get_parent());
+			if (parent_collider) {
+				set_mesh(parent_collider->get_shape()->get_debug_mesh());
+			}
+			break;
+		}
+		case NOTIFICATION_UNPARENTED: {
+			parent_collider = nullptr;
+			set_mesh(nullptr);
+			break;
+		}
+	}
 }
-void Stupid4Pt5Mesh::set_parent_collider(CollisionShape3D p_parent_collider) {
-	parent_collider = p_parent_collider;
+
+Stupid4Pt5Mesh* Stupid4Pt5Mesh::create(CollisionShape3D* p_collider, bool p_interp) {
+	Stupid4Pt5Mesh* stupid_mesh = memnew(Stupid4Pt5Mesh);
+	stupid_mesh->set_physics_interpolation_mode(p_interp? PHYSICS_INTERPOLATION_MODE_ON : PHYSICS_INTERPOLATION_MODE_OFF);
+	p_collider->add_child(stupid_mesh);
 }
+
 void Stupid4Pt5Mesh::_bind_methods(){
-ClassDB::bind_method(D_METHOD("_init", "collider", "interp"), &Stupid4Pt5Mesh::_init, DEFVAL(false));
-ClassDB::bind_method(D_METHOD("_physics_process", "_delta"), &Stupid4Pt5Mesh::_physics_process);
-ClassDB::bind_method(D_METHOD("update_transform"), &Stupid4Pt5Mesh::update_transform);
-ClassDB::bind_method(D_METHOD("set_parent_collider", "value"), &Stupid4Pt5Mesh::set_parent_collider);
-ClassDB::bind_method(D_METHOD("get_parent_collider"), &Stupid4Pt5Mesh::get_parent_collider);
-ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "parent_collider"), "set_parent_collider", "get_parent_collider"); // unfinished and u should prolly change this
+	ClassDB::bind_static_method("Stupid4Pt5Mesh",D_METHOD("create","collider","interpolate"),&Stupid4Pt5Mesh::create);
+	// ClassDB::bind_method(D_METHOD("update_transform"), &Stupid4Pt5Mesh::update_transform);
+//	ClassDB::bind_method(D_METHOD("set_parent_collider", "value"), &Stupid4Pt5Mesh::set_parent_collider);
+//	ClassDB::bind_method(D_METHOD("get_parent_collider"), &Stupid4Pt5Mesh::get_parent_collider);
+	// ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "parent_collider"), "set_parent_collider", "get_parent_collider"); // unfinished and u should prolly change this
 }
--- modules/multiplayer/miles/stupid_4_pt_5_mesh.h
+++ modules/multiplayer/miles/stupid_4_pt_5_mesh.h
@@ -1,13 +1,16 @@
+#include "scene/3d/physics/collision_shape_3d.h"
+#include "scene/3d/mesh_instance_3d.h"
 
 class Stupid4Pt5Mesh: public MeshInstance3D {
-GDCLASS(Stupid4Pt5Mesh, MeshInstance3D);
-public:
-CollisionShape3D parent_collider;
-void _init(CollisionShape3D collider, bool interp);
-void _physics_process(float _delta);
-void update_transform();
-CollisionShape3D get_parent_collider();
-void set_parent_collider(CollisionShape3D p_parent_collider);
-static void _bind_methods();
-Stupid4Pt5Mesh();~Stupid4Pt5Mesh();
+    GDCLASS(Stupid4Pt5Mesh, MeshInstance3D);
+    public:
+        CollisionShape3D* parent_collider;
+        void update_transform();
+        void Stupid4Pt5Mesh::_notification(int what);
+        // CollisionShape3D get_parent_collider();
+        // void set_parent_collider(CollisionShape3D p_parent_collider);
+        static void _bind_methods();
+        static Stupid4Pt5Mesh* create(CollisionShape3D* p_collider, bool p_interp);
+        Stupid4Pt5Mesh();
+        // ~Stupid4Pt5Mesh();
 };
